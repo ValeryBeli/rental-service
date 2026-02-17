46feb356d09e24b6fbfa91bc148b2ba72c8c2f09
ValeryBeli <murlerohka@gmail.com>
2026-02-17 19:12:29 +0300

    Загружена лабораторная работа 2

--- server/index.js
+++ server/index.js
@@ -5,14 +5,25 @@ import Offer from './models/offer.js';
 import Review from './models/review.js';
 import User from './models/user.js';
 import router from './routes/index.js';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
 app.use('/', router);
+app.use(errorMiddleware);
 
 const start = async () => {
     try {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 import {Router} from 'express';
 import offerRoute from './offerRoutes.js';
+import userRotes from './userRoutes.js';
 
 const router = new Router();
+
 router.use('/', offerRoute);
+router.use('/', userRotes);
 
 export default router;
